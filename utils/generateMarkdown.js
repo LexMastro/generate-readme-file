736e6175c40d0d5ea0160c78a956578e35748ab7
Alexis Mastrodomenico <86946841+LexMastro@users.noreply.github.com>
2021-10-21 17:38:52 +1100

    1st commit

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,39 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  const link = `https://img.shields.io/badge/License-${license}-blue`
+  return `![${license}]()`
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(answers) {
+  const template = `# ${answers.title}
+# Description
+${answers.desc}
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Instrcutions](#instructions)
+- [License](#license)
+
+# Installation
+${answers.desc}
+
+# Instructions
+${answers.desc}
+
+## License
+${renderLicenseLink(answers.license)}
+`;
+}
+
+module.exports = generateMarkdown;
